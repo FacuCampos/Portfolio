e718e4426478ef4ec0f8f29eb8335312feba4de9
FacuCampos <facujcampos@gmail.com>
2024-05-14 00:08:58 -0300

    Se corrigio el desplegable de certificados

--- src/components/Certificaciones/index.jsx
+++ src/components/Certificaciones/index.jsx
@@ -26,7 +26,7 @@ const Certificaciones = () => {
     verMas ? setBoton({ texto: "Ocultar" }) : setBoton({ texto: "Ver todos" });
 
     const certDropdown = document.querySelector(".certificaciones-contenedor");
-    certDropdown.classList.toggle("mostrarRows");
+    verMas && certDropdown.classList.toggle("mostrarRows");
   }, [verMas]);
 
   return (
