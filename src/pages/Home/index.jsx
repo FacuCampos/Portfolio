524f4f390552e5e4f52e01786246394ed163d9b2
FacuCampos <facujcampos@gmail.com>
2024-09-11 16:41:20 -0300

    Se corrigió error en la alerta de idioma

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -19,36 +19,37 @@ const Home = () => {
 
   useEffect(() => {
     MySwal.fire({
-      title: <h2>{i18n.language === "en" ? "Sent!" : "¡Enviado!"}</h2>,
-      html: (
-        <p>
-          {i18n.language === "en"
-            ? "The message has been sent successfully"
-            : "El mensaje ha sido enviado exitosamente"}
-        </p>
-      ),
-      buttonsStyling: false,
-      allowOutsideClick: false,
-      allowEnterKey: false,
-      allowEscapeKey: false,
+      html: `
+      <div class="toastDiv">
+        <p>Cambia el idioma a <strong>Español</strong> desde aquí</p>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"/>
+        </svg>
+      </div>
+      `,
+      toast: true,
       stopKeydownPropagation: false,
+      timer: window.innerWidth <= 992 ? 2000 : 5000,
+      timerProgressBar: true,
+      position: "top-right",
       showCloseButton: true,
+      showConfirmButton: false,
+      customClass: {
+        popup: "popupClass",
+      },
       showClass: {
         popup: `
           animate__animated
-          animate__zoomIn
+          animate__fadeInUp
           animate__faster
         `,
       },
       hideClass: {
         popup: `
           animate__animated
-          animate__zoomOut
+          animate__backOutRight
           animate__faster
         `,
-      },
-      customClass: {
-        popup: "emailSendToast",
       },
     });
   }, []);
